a2b901de6f04acd942270cbd2352fe23e0a70dc0
Pavel Appolonov <app.pavel1998@gmail.com>
2024-07-25 23:57:49 +0300

    Node класс создан отдельный файлом .class

--- src/historytracking/InMemoryHistoryManager.java
+++ src/historytracking/InMemoryHistoryManager.java
@@ -94,14 +94,4 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 }
 
-class Node<Task> {
-    public Task task;
-    public Node<Task> next;
-    public Node<Task> prev;
 
-    public Node(Task task) {
-        this.task = task;
-        this.next = null;
-        this.prev = null;
-    }
-}
